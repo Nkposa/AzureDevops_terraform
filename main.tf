5fa895492a1c7a17b1e32fcfd9e5fca727cbbdbb
NaveenP <Naveen.Posa@thomsonreuters.com>
2025-11-23 16:55:32 +0530

    added

---
+++ main.tf
@@ -0,0 +1,78 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_storage_account_type
+  }
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+}
+resource "azurerm_storage_account" "tfstate" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                 = var.storage_container_name
+  storage_account_id   = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "tfstate" {
+  name                   = var.tfstate_blob_name
+  storage_account_name   = azurerm_storage_account.tfstate.name
+  storage_container_name = azurerm_storage_container.tfstate.name
+  type                   = "Block"
+}
---
+++ variables.tf
@@ -0,0 +1,112 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "South India"
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "Address prefix for the subnet"
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+}
+
+variable "nic_name" {
+  description = "Name of the network interface"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+  default     = "Standard_F2"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "adminuser"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to the SSH public key file"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "os_disk_caching" {
+  description = "Caching type for OS disk"
+  type        = string
+  default     = "ReadWrite"
+}
+
+variable "os_disk_storage_account_type" {
+  description = "Storage account type for OS disk"
+  type        = string
+  default     = "Standard_LRS"
+}
+
+variable "image_publisher" {
+  description = "Publisher of the VM image"
+  type        = string
+  default     = "Canonical"
+}
+
+variable "image_offer" {
+  description = "Offer of the VM image"
+  type        = string
+  default     = "0001-com-ubuntu-server-jammy"
+}
+
+variable "image_sku" {
+  description = "SKU of the VM image"
+  type        = string
+  default     = "22_04-lts"
+}
+
+variable "image_version" {
+  description = "Version of the VM image"
+  type        = string
+  default     = "latest"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account for Terraform state"
+  type        = string
+}
+
+variable "storage_container_name" {
+  description = "Name of the storage container for Terraform state"
+  type        = string
+}
+
+variable "tfstate_blob_name" {
+  description = "Name of the Terraform state blob file"
+  type        = string
+  default     = "terraform.tfstate"
+}
